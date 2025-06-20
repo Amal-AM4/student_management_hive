76cc0c4a15e8d1a557b612f7b3313a9e3bf97961
Amal A M <vectorhero007@gmail.com>
2025-06-20 12:01:53 +0530

    this code is type from chatgpt

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import '../models/student_model.dart';
 
+import 'add_student_screen.dart';
+import 'student_detail_screen.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  // const HomeScreen({super.key});
+
+  final studentsBox = Hive.box<StudentModel>('studentsBox');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('test')),
+      appBar: AppBar(
+        title: Text(
+          'Student List',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      body: SafeArea(
+        ValueListenableBuilder(
+          valueListenable: studentsBox.listenable(),
+          builder: (context, Box<StudentModel> box, _) {
+            if (box.isEmpty) return Center(child: Text("No Students Yet!"),);
+            return ListView.builder(
+              itemCount: box.length,
+              itemBuilder: () {
+                final student = box.getAt(index)!;
+                return ListTile(
+                  title: Text(student.name),
+                  subtitle: Text("Age: ${student.age} | Course: ${student.course} | State: ${student.state} | UUID: ${student.id}"),
+                  onTap: () {
+                    Navigator.push(context, 
+                    MaterialPageRoute(
+                      builder: (_) => StudentDetailScreen(index: index, student: student,)
+                    ),
+                    );
+                  },
+                ),
+              });
+          },
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {
+          Navigator.push(context, 
+          MaterialPageRoute(builder: (_) => AddStudentScreen());
+          )
+        },
+        child: Icon(Icons.add),
+        )
+      ),
     );
   }
 }
---
+++ lib/screens/student_detail_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class StudentDetailScreen extends StatelessWidget {
+  // const StudentDetailScreen({super.key});
+
+  late var index;
+  late var student;
+
+  StudentDetailScreen({required this.index, required this.student});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
